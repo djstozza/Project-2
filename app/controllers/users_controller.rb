1bcccb574abf45af4a4e7ab9090e1c8ba0f8eca8
djstozza <daniel.sztolcman@gmail.com>
2015-09-25 14:06:51 +1000

    customising taskbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,23 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :check_if_logged_in, :only => [:show, :edit, :update]
+  # before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :check_if_admin, :only => [:index]
+
+
 
   # GET /users
   # GET /users.json
   def index
     @users = User.all
+    unless @current_user.present? && @current_user.admin?
+      redirect_to root_path
+    end
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find params[:id]
   end
 
   # GET /users/new
@@ -66,9 +72,9 @@ class UsersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+    # def set_user
+    #   @user = User.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
