c99e60c5edacf1bcac40ab7398ed2fc6aa4bc180
djstozza <daniel.sztolcman@gmail.com>
2015-09-29 16:13:18 +1000

    add for sale and jobs to items + further form customisation

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,34 +2,70 @@
 #
 # Table name: items
 #
-#  id               :integer          not null, primary key
-#  name             :string
-#  price            :float
-#  subcategory_id   :integer
-#  user_id          :integer
-#  category_id      :integer
-#  image            :string
-#  description      :string
-#  created_at       :datetime
-#  updated_at       :datetime
-#  private_room     :string
-#  private_bathroom :string
-#  rooms            :integer
-#  bathrooms        :integer
-#  parking          :string
-#  laundry          :string
-#  rent             :integer
-#  sale             :integer
-#  housing_type     :string
-#  area             :integer
-#  available        :date
-#  openhouse1       :datetime
-#  openhouse2       :datetime
-#  openhouse3       :datetime
-#  pets             :boolean
-#  furnished        :boolean
-#  smoking          :boolean
-#  wheelchair       :boolean
+#  id                  :integer          not null, primary key
+#  name                :string
+#  price               :float
+#  subcategory_id      :integer
+#  user_id             :integer
+#  category_id         :integer
+#  image               :string
+#  description         :string
+#  created_at          :datetime
+#  updated_at          :datetime
+#  private_room        :string
+#  private_bathroom    :string
+#  rooms               :integer
+#  bathrooms           :integer
+#  parking             :string
+#  laundry             :string
+#  rent                :integer
+#  sale                :integer
+#  housing_type        :string
+#  area                :integer
+#  available           :date
+#  openhouse1          :datetime
+#  openhouse2          :datetime
+#  openhouse3          :datetime
+#  pets                :boolean
+#  furnished           :boolean
+#  smoking             :boolean
+#  wheelchair          :boolean
+#  employment_type     :string
+#  salary              :string
+#  recruiter           :boolean
+#  internship          :boolean
+#  non_profit          :boolean
+#  telecommuting       :boolean
+#  disability          :boolean
+#  make                :string
+#  condition           :string
+#  model               :string
+#  dimensions          :string
+#  serial_number       :string
+#  engine_hours        :integer
+#  length_overall      :integer
+#  propulsion_type     :string
+#  model_year          :date
+#  vin                 :string
+#  cylinders           :string
+#  drive               :string
+#  fuel                :string
+#  engine_displacement :string
+#  paint_colour        :string
+#  size                :string
+#  title_status        :string
+#  transmission        :string
+#  car_type            :string
+#  media_type          :string
+#  mobile_os           :string
+#  garage_sale1        :date
+#  garage_sale2        :date
+#  garage_sale3        :date
+#  start_time          :time
+#  odometer            :integer
+#  event               :datetime
+#  tickets             :integer
+#  venue               :string
 #
 
 class Item < ActiveRecord::Base
@@ -38,7 +74,6 @@ class Item < ActiveRecord::Base
 	has_one :bookmark
 
 	validates :name, :presence => true
-	validates :price, :presence => true
 	validates :description, :presence => true
 	validates :category_id, :presence => true
 	validates :subcategory_id,  :presence => true
